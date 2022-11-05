3bf88f840e420f0ae9f1c75db737f0621e35ae7c
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-11-05 16:49:42 +0500

    dynamic header links

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { nav } from '../../data/Data'
 import './header.css'
 
@@ -8,7 +8,7 @@ export default function Header() {
     const [navList, setNavList] = useState(false)
 
     const toggleNavList = () => {
-        if(navList) {
+        if (navList) {
             setNavList(false)
         }
         else {
@@ -17,44 +17,53 @@ export default function Header() {
     }
 
     const closeNavList = () => {
-        if(navList) {
+        if (navList) {
             setNavList(false)
         }
     }
 
-  return (
-    <>
-        <header>
-            <div className="container flex">
-                <div className="logo">
-                    <img src="/images/logo.png" alt="" />
+    const location = useLocation()
+
+    return (
+        <>
+            <header>
+                <div className="container flex">
+                    <div className="logo">
+                        <img src="/images/logo.png" alt="" />
+                    </div>
+                    <div className="nav">
+                        <ul className={navList ? 'small' : 'flex'}>
+                            {
+                                nav.map((item, index) => {
+                                    console.log(item.path, location.pathname)
+                                    return (
+                                        <li key={index}>
+                                            <Link
+                                                to={item.path}
+                                                onClick={closeNavList}
+                                                style={{color: item.path === location.pathname ? '#27ae60' : '#000'}}
+                                            >
+                                                {item.text}
+                                            </Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className="button flex">
+                        <button className="btn1">
+                            <i className="fa fa-sign-out"></i>
+                            Sign In
+                        </button>
+                    </div>
+                    <div className="toggle">
+                        <button onClick={toggleNavList}>
+                            <i className={navList ? 'fa fa-times' : 'fa fa-bars'} style={{ marginRight: 0 }}></i>
+                        </button>
+                    </div>
                 </div>
-                <div className="nav">
-                    <ul className={navList ? 'small' : 'flex'}>
-                        {
-                            nav.map((item, index) => {
-                                return (
-                                    <li key={index}>
-                                        <Link to={item.path} onClick={closeNavList}>{item.text}</Link>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="button flex">
-                    <button className="btn1">
-                        <i className="fa fa-sign-out"></i>
-                        Sign In
-                    </button>
-                </div>
-                <div className="toggle">
-                    <button onClick={toggleNavList}>
-                        <i className={navList ? 'fa fa-times' : 'fa fa-bars'} style={{marginRight: 0}}></i>
-                    </button>
-                </div>
-            </div>
-        </header>
-    </>
-  )
+            </header>
+        </>
+    )
 }
